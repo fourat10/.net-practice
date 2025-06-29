399026354796d91b51fcf5bc7f045b7c72b8feaa
fourat <fouratmarouen0@gmail.com>
2025-06-29 16:19:59 +0100

    first commit

---
+++ API/Controllers/DepartmentController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DepartmentController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetAllDepartment()
+        {
+            return Ok("get all students");
+        }
+        // Route Param
+        [HttpGet("{id}")]
+        public IActionResult GetDepartmentById(Guid id)
+        {
+            return Ok("get this dep = id  = "+id.ToString());
+        }
+        /*  Query Param
+         [HttpGet]
+        public IActionResult GetDepartmentById([FromQuery] Guid id)
+        {
+            return Ok($"get this dep = id = {id}");
+        }
+
+         */
+
+        [HttpPost("{code}")]
+        public IActionResult PostCommentaire(string code)
+        {
+           return Ok("updated sussuful "+code);
+        }
+
+        [HttpDelete]
+        public IActionResult DeleteCommentaire()
+        {
+            return Ok("deleted sussuful ");
+        }
+    }
+}
